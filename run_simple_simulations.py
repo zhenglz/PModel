f432fa69214d2b09dc2f952ea5a2c1fb9696411e
zlzzheng <zlzzheng@tencent.com>
2019-05-27 22:30:00 +0800

    add dihedral restraints

--- run_simple_simulations.py
+++ run_simple_simulations.py
@@ -4,12 +4,80 @@ from simtk import openmm as mm
 from simtk import unit as u
 #from sys import stdout
 from datetime import datetime
+import pandas as pd
 import argparse
+import os
 from argparse import RawDescriptionHelpFormatter
 
 
-def dihedral_restraints(rst="restraints.txt", use_cols=[0, 1, 2, 3, -2, -1]):
-    pass
+class ParseConstraints:
+
+    def __init__(self, cst_type="dihedral", filen="restraint.txt"):
+        self.cst_type = cst_type
+        self.cst_file = filen
+        self.cst_df = pd.DataFrame()
+        self.forces = None
+
+    def read_restraints(self):
+        with open(self.cst_file) as lines:
+
+            self.cst_df = pd.DataFrame([x.split() for x in lines])
+
+        return self
+
+    def force_type(self):
+        if self.cst_type == "dihedral":
+            self.forces = mm.CustomTorsionForce('0.5*k*(theta-theta0)^2')
+            self.forces.addPerTorsionParameter('theta0',)
+            self.forces.addPerTorsionParameter('k',)
+
+        elif self.cst_type == "distance":
+            self.forces = mm.CustomBondForce('0.5*k*(r-r0)^2')
+            self.forces.addPerTorsionParameter('r0',)
+            self.forces.addPerTorsionParameter('k',)
+
+        else :
+
+            self.forces == mm.CustomAngleForce('0.5*k*(d-d0)^2')
+
+        return self
+
+    def add_forces(self):
+        if self.cst_type == "dihedral":
+            for items in self.cst_df.values:
+                atom_index_i = int(items[0])
+                atom_index_j = int(items[1])
+                atom_index_k = int(items[2])
+                atom_index_l = int(items[3])
+
+                r0 = float(items[-3])
+                k = float(items[-1])
+
+                self.forces.addTorsion(atom_index_i, atom_index_j,
+                                             atom_index_k, atom_index_l,
+                                             [r0, k])
+        if self.cst_type == "distance":
+            for items in self.cst_df.values:
+                atom_index_i = int(items[0])
+                atom_index_j = int(items[1])
+                #atom_index_k = int(items[2])
+                #atom_index_l = int(items[3])
+
+                r0 = float(items[-3])
+                k = float(items[-1])
+
+                self.forces.addBond(atom_index_i, atom_index_j, [r0, k])
+
+        return self
+
+def dihedral_restraints(rst="restraints.txt", use_cols=[0, 1, 2, 3, -2, -1], rst_type="dihedral"):
+
+    restraints = ParseConstraints(rst_type, rst)
+    restraints.read_restraints()
+    restraints.force_type()
+    restraints.add_forces()
+
+    return restraints.forces
 
 
 def process_pdb(inp, addH=False, ):
@@ -18,7 +86,7 @@ def process_pdb(inp, addH=False, ):
 
     return pdb
 
-def prepare_system(pdb, gbsa=False):
+def prepare_system(pdb, gbsa=False, add_forces=None):
 
     if gbsa:
 
@@ -37,6 +105,9 @@ def prepare_system(pdb, gbsa=False):
         system = forcefield.createSystem(pdb.topology, nonbondedMethod=PME,
                                          nonbondedCutoff=1*u.nanometer,
                                          constraints=HBonds)
+
+    if add_forces is not None:
+        system.addForce(add_forces)
 
     return system
 
@@ -86,6 +157,8 @@ if __name__ == "__main__":
                              "Default is True. ")
     parser.add_argument("--temperature", default=300, type=int,
                         help="Input, int, optional. The simulation temperature, default is 300 K. ")
+    parser.add_argument("--diherst", default=None, type=str,
+                        help="Input, str, optional. Apply dihedral restraints to the system. ")
 
     args = parser.parse_args()
 
@@ -96,7 +169,12 @@ if __name__ == "__main__":
 
     pdb = process_pdb(args.pdbin)
 
-    system = prepare_system(pdb, gbsa=args.gbsa)
+    if args.diherst is not None and os.path.exists(args.diherst):
+        forces = dihedral_restraints(args.diherst, rst_type="dihedral")
+    else:
+        forces = None
+
+    system = prepare_system(pdb, gbsa=args.gbsa, add_forces=forces)
 
     run_NVT(pdb, system=system, out=args.pdbout, log=args.logfile,
             nsteps=args.nsteps, temperature=args.temperature)
